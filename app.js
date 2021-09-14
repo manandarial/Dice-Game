303a15913e830dcdfbde6a931e5ed420e671ca1f
Abdul Manan <manan.8992@gmail.com>
2021-09-14 11:47:42 +0530

    added winning score logic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let player1Score = 0
 let player2Score = 0
 let player1Turn = true
+let winningScore = 20
 
 const player1Dice = document.getElementById('player1Dice')
 const player2Dice = document.getElementById('player2Dice')
@@ -9,6 +10,8 @@ const player2Scoreboard = document.getElementById('player2Scoreboard')
 const message = document.getElementById('message')
 const rollBtn = document.getElementById('rollBtn')
 const resetBtn = document.getElementById('resetBtn')
+const inputNumber = document.querySelector('input')
+const winningScoreDisplay = document.querySelector('h1 span')
 
 function showResetBtn(){
     rollBtn.style.display = 'none'
@@ -23,21 +26,21 @@ rollBtn.addEventListener('click', function(){
         player1Score += randomNumber
         player1Scoreboard.textContent = player1Score
         player1Dice.textContent = randomNumber
-        player1Dice.classList.remove("active")
-        player2Dice.classList.add("active")
+        player1Dice.classList.remove('active')
+        player2Dice.classList.add('active')
         message.textContent = 'Player 2 Turn'
     } else {
         player2Score += randomNumber
         player2Scoreboard.textContent = player2Score
         player2Dice.textContent = randomNumber
-        player2Dice.classList.remove("active")
-        player1Dice.classList.add("active")
+        player2Dice.classList.remove('active')
+        player1Dice.classList.add('active')
         message.textContent = 'Player 1 Turn'
     }
-    if(player1Score >= 20){
+    if(player1Score >= winningScore){
         message.textContent = 'Player 1 WON 🎖'
         showResetBtn()
-    }else if(player2Score >= 20){
+    }else if(player2Score >= winningScore){
         message.textContent = 'Player 2 WON 🎖'
         showResetBtn()
     }
@@ -62,3 +65,10 @@ function reset(){
     player2Dice.classList.remove('active')
     player1Dice.classList.add('active')
 }
+
+inputNumber.addEventListener('change', function(){
+    winningScoreDisplay.textContent = inputNumber.value
+    winningScore = inputNumber.value
+    inputNumber.textContent = inputNumber.value
+    reset()
+})
